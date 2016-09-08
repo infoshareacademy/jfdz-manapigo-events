21782d4888ed25096a0b9efb943913aad8c9245d
Natslu <slugocka.n@gmail.com>
2016-09-08 13:15:36 +0200

    gra "statki" - poczatek wraz z opisami FYI

---
+++ statki/statki.js
@@ -0,0 +1,38 @@
+/**
+ * Created by Nat on 2016-09-06.
+ */
+var randomLoc = Math.floor(Math.random() * 5);  //math.random to losowo generowana liczba z zakresu 0 - 1. Przy przemnozeniu jej przez 5 zakres zwieksza sie do 4.999... . Math.floor to liczba w postaci calkowitej a nie w ulamku
+var location1 = randomLoc; //zmienne do przechowywania pozycji okretu - losowo wybrane polozenie i 2 pola obok
+var location2 = location1 + 1;
+var location3 = location2 + 1;
+
+var guess; //zmienna do przechowywania pola wskazanego przez uzytkownika        |
+//                                                                              |
+var hits = 0; //zmienna do przechowywania liczby trafien                         =====> proby zatopienia okretu   
+//                                                                              |
+var guesses = 0; //zmienna do przechowywania liczby prob                        |
+
+var isSunk = false; //zmienna do przechowywania informacji czy zatopilam okret
+
+while (isSunk == false) {   //petla, ktora pobiera pole do sprawdzenia
+    guess = prompt("Fire! (choose a number between 0 and 6");   //przy zamknieciu kna lub gdy nic nie zostanie wpisane petla zwraca NULL
+    if (guess < 0 || guess > 6) {   // sprawdzenie czy uzytkownik podal liczbe z zakresu 0-6
+        alert("Put yourself into gear soldier! The order was NUMBERS FROM 0 TO 6!");    //komunikat 'nieprawidlowa liczba zostala podana
+    }
+    else {
+        guess = guess + 1;  //podana prawidlowa liczba. dodaje 1 zeby liczba prob sie zgadzala 
+
+        if (guess == location1 || guess == location2 || guess == location3) {
+            hits = hits + 1;    //inkrementacja = uzytkownik trafil
+            if (hits == 3) {    //zatopienie okretu
+                isSunk = true;
+                alert("Well done soldier, mission accomplished!");
+            }
+        }
+    }
+}
+
+var stats = "It took you" + guesses + "goes to sunk the ship" + "the score is: " + (3 / guesses) + ".";
+
+alert (stats);
+
